fda4163a90aee0c201e74f6862a186ad81e1608a
Camilahig <camilah.lac@gmail.com>
2025-04-10 13:07:38 -0600

    Add files via upload

---
+++ Ejemplo02/src/Estudiante.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Estudiante {
+//atributos caracteristicas o lo que define al estudiante
+       String nombre;
+       int edad;
+       // metodo son acciones que puede realizar el estudiante
+    public void saludar() {
+        System.out.println("Hola: mi nombre es valeria y tengo 23 años");
+    }
+
+}
---
+++ Ejemplo02/src/Principal.java
@@ -0,0 +1,9 @@
+public class Principal {
+    public static void main(String[] args) {
+        Estudiante estudiante1 = new Estudiante();
+        estudiante1.nombre="valeria";
+        estudiante1.edad=23;
+        estudiante1.saludar();
+
+    }
+}
